0c4e025dc8ffe483a4d9ffe5a8b5111242655c30
Dalton Brua <daltonbrua@gmail.com>
2024-11-07 23:18:13 -0600

    app.py can insert data to MongoDB. Need to work on getting data next

---
+++ Intellihub/backend/README.md
@@ -0,0 +1,9 @@
+# Install and Create Environment
+
+Navigate to /Intellihub/backend and run these commands
+
+### py -3 -m venv .venv
+### .venv/Scripts/activate
+### pip install flask
+### pip install pymongo
+### flask --app app run
---
+++ Intellihub/backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")
+
+db = client["KMA_DB"]
+
+@app.route('/data')
+def createUser():
+    collection = db.users
+    post = {
+            "name": "John Doe",
+            "pass": "pass1",
+            "role": "user"
+        }
+    ##result = collection.insert_one(post)
+    ##return 'Inserted document with id: {}'.format(result.inserted_id)
+    return {
+            "name": "John Doe",
+            "pass": "pass1",
+            "role": "user"
+        }
