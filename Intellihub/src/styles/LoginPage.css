a12a1a45cfdb18b1630fc6396a1f7e48d11c8e2e
Ashley Primrose <116143986+Primrizzle@users.noreply.github.com>
2024-11-13 20:48:43 -0600

    Create LoginPage.css
    Added Login Page Style

---
+++ Intellihub/src/styles/LoginPage.css
@@ -0,0 +1,68 @@
+import React from "react";
+import { DashboardSideNav } from "./DashboardSideNav";
+import { Document } from "./Document";
+import { Logout } from "./Logout";
+import { Setting } from "./Setting";
+import ellipse5 from "./ellipse-5.png";
+import "./style.css";
+
+export const SideNav = () => {
+  return (
+    <div className="side-nav">
+      <div className="div-2">
+        <img className="ellipse" alt="Ellipse" src={ellipse5} />
+
+        <div className="logout-toogle">
+          <div className="leading-icon-nav-wrapper">
+            <div className="leading-icon-nav-2">
+              <div className="div-3">
+                <Logout className="icon-instance-node" color="white" />
+              </div>
+
+              <div className="div-3">
+                <div className="text-wrapper">Logout</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="brand-nav-list">
+          <div className="nav-brand" />
+
+          <div className="nav-list">
+            <DashboardSideNav
+              className="dashboard-side-nav-button"
+              property1="default"
+              text="Home"
+            />
+            <div className="div-wrapper">
+              <div className="leading-icon-nav-2">
+                <div className="div-3">
+                  <Document className="icon-instance-node" />
+                </div>
+
+                <div className="div-3">
+                  <div className="dashboard-2">Saved Queries</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="div-wrapper">
+              <div className="leading-icon-nav-2">
+                <div className="div-3">
+                  <Setting className="icon-instance-node" color="#EFF2F4" />
+                </div>
+
+                <div className="div-3">
+                  <div className="dashboard-2">Settings</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="text-wrapper-2">Intellihub</div>
+      </div>
+    </div>
+  );
+};
