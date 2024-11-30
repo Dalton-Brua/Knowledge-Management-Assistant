d297ce5ca739894fc59d0b487b3e40c95a842499
Dalton Brua <daltonbrua@gmail.com>
2024-11-30 16:58:26 -0600

    Added user column back

--- Intellihub/src/components/HistoryTable.jsx
+++ Intellihub/src/components/HistoryTable.jsx
@@ -16,12 +16,14 @@ export const HistoryTable = () => {
             <div className="table-header">
                 <div className="header-cell">Query</div>
                 <div className="header-cell">Date</div>
+                <div className="header-cell">User</div>
             </div>
             <div className="table-body">
                 {tableData.map((row, index) => (
                     <div key={index} className="table-row">
                         <div className="table-cell">{row.query}</div>
                         <div className="table-cell">{row.timestamp}</div>
+                        <div className="table-cell">{row.user}</div>
                     </div>
                 ))}
             </div>
