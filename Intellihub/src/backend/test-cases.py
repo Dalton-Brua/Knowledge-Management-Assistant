937709d2be0488a7be21f9cc9121483dfeef6a55
Ashley Primrose <116143986+Primrizzle@users.noreply.github.com>
2024-12-02 21:57:43 -0600

    Update test-cases.py
    Updated from Roj and Dalton's code

--- Intellihub/src/backend/test-cases.py
+++ Intellihub/src/backend/test-cases.py
@@ -1,23 +1,25 @@
-
 from search import search
-from gemini_api import GeminiSeummarizer
+from gemini_api import GeminiSummarizer
+from dotenv import load_dotenv
+import os
 
-API_KEY = 'AIzaSyDWjmv5nxuxjcO7xEkE2uG_uwS13SvAsIE'
-SEARCH_ENGINE_ID = '0546012e6548e4e3f'
-google_api_key = "AIzaSyAZJbOEfC101tST3VcpknqSHJVmubhn0DE"
+load_dotenv()
+API_KEY = os.getenv('SEARCH_API_KEY')
+SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID')
+google_api_key = os.getenv('GOOGLE_API_KEY')
 
 # Test Query
-query = "Best Black Friday Deals"
+query = "What programming languages should we utilize in 2024 for a particular new software system?"
 
 # save search results
 search_output_file = "search_results.json"
-search(query, API_KEY, SEARCH_ENGINE_ID, num_results = 10, output_file = search_output_file)
+search(query, num_results = 10, output_file = search_output_file)
 
 # sumamrize search results
-summarization_outputfile = "gemini_results.json"
-summarizer = GeminiSeummarizer(api_key=google_api_key)
-summarizer.summarize_results(search_output_file,summarization_outputfile)
+summary_output_file = "summary_results.json"
+summarizer = GeminiSummarizer()
+summarizer.summarize_results(input_file=search_output_file, output_file=summary_output_file, query=query)
 
 
 
-print(f"Summarization completed. Results saved to {summarization_outputfile}")
+#print(f"Summarization completed. Results saved to {summarization_outputfile}")
