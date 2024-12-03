76b7c9ea6669a21a9bf51c51ee9e0b7e6dc1ce78
Ashley Primrose <116143986+Primrizzle@users.noreply.github.com>
2024-12-02 21:57:08 -0600

    Update search.py
    Updated to Roj and Dalton's code

--- Intellihub/src/backend/search.py
+++ Intellihub/src/backend/search.py
@@ -1,29 +1,28 @@
 import json
+import os
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
+import os
+from dotenv import load_dotenv
 
 #TODO: Store as environment variables
-API_KEY = 'AIzaSyDWjmv5nxuxjcO7xEkE2uG_uwS13SvAsIE'
-SEARCH_ENGINE_ID = '0546012e6548e4e3f'
+load_dotenv()
 
-def search(query, api_key, search_engine_id, num_results=10, output_file="search_results.json"):
+API_KEY = os.getenv('SEARCH_API_KEY')
+SEARCH_ENGINE_ID = os.getenv('SEARCH_ENGINE_ID')
+
+def search(query, num_results=10, output_file="search_results.json"):
     try:
-        service = build("customsearch", "v1", developerKey=api_key) # Builds Google Custom Search API service
+        service = build("customsearch", "v1", developerKey=API_KEY) # Builds Google Custom Search API service
         results = []
         for start_index in range(1, num_results + 1, 10):  # Max 10 results per page
             response = (
                 service.cse()
-                .list(q=query, cx=search_engine_id, start=start_index)
+                .list(q=query, cx=SEARCH_ENGINE_ID, start=start_index)
                 .execute()
             )
             results.extend(response.get("items", []))
         with open(output_file, "w") as file: # Saves results to JSON file
-            json.dump(results, file, indent=4)  #TODO: Use links from each result and insert into AI service to summarize and analyze results for a response
+            json.dump(results, file, indent=4)
     except Exception as e:
         print(f"An error occurred: {e}")
-
-# Test usage
-if __name__ == "__main__":
-    # Test query
-    query = "Best Black Friday Deals" #TODO: Connect with frontend to get query from dashboard search form.
-    
-    search(query, API_KEY, SEARCH_ENGINE_ID, num_results=20)
